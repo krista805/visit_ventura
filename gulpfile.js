b162fbbb49c00691a3e3132d25a5c8fb9c62921c
Krista Simmons <kristasimmons.design@gmail.com>
2018-04-22 09:17:09 -0700

    placed source files into root

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ const sass        = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['src/scss/*.scss'])
+    return gulp.src(['scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
 });
 
@@ -14,11 +14,11 @@ gulp.task('sass', function() {
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        server: "./src"
+        server: ""
     });
 
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['scss/*.scss'], ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Default Task
